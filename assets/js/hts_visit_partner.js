a96109f88cb610bc4519da8759fda3cc813572f7
towera <towera.moyo@yahoo.com>
2019-07-16 09:28:07 -0400

    Changed to application.json file

--- assets/js/hts_visit_partner.js
+++ assets/js/hts_visit_partner.js
@@ -12,21 +12,14 @@ function buildSplit(){
     div.style.borderRight = "2px solid red";
     div.id = "hts_visit";
 
+    var ifrm = document.createElement('iframe');
+    ifrm.setAttribute('id', 'ifrm'); // assign an id
+    ifrm.setAttribute('src', 'hts_visit.html');
+
 
     frame.appendChild(div);
-    buildHTS();
+    bildHTS();
 }
 function buildHTS(){
-    // var form = document.getElementById('form');
-    // var input_ht = document.createElement("input");
-    // input_ht.id = "hts_access_type";
-    // input_ht.setAttribute("name","test_date");
-    // input_ht.setAttribute("tt_pageStyleClass","Numeric NumbersOnly");
-    // input_ht.setAttribute("helpText","Number of male condoms given");
-    // form.appendChild(input_ht);
-    var temp = document.getElementsByTagName("template")[0];
-    var clon = temp.content.cloneNode(true);
-    frame.appendChild(clon);
-    //input_ht.setAttribute("")
-    //  document.getElementById("hts_visit").innerHTML ='<object type="text/html" data="hts_visit.html"> </object>';
+    
 }
