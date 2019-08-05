c1d8b06256bd61e440b63a016a1725a5489d783e
towera <towera.moyo@yahoo.com>
2019-08-05 16:32:36 -0400

    Retrieving user statistics data

--- assets/js/user_stats.js
+++ assets/js/user_stats.js
@@ -17,15 +17,16 @@ var apiURL = sessionStorage.apiURL;
 var url = apiProtocol + "://" + apiURL + ":" + apiPort + "/api/v1";
 
 var userDate, firstName, lastName, username, total;
+var locationName = " ";
 
 $j(document).ready(function(){
 
     populateTable(start_date,end_date);
  
- });
+  });
  
  
-   function populateTable(start_date,end_date){
+  function populateTable(start_date,end_date){
      
 
      var url = apiProtocol + "://" + apiURL + ":" + apiPort;
@@ -70,11 +71,32 @@ $j(document).ready(function(){
       firstName = data['given_name'];
       total = data['total_visits'];
       username = data['username'];
-    
+     var location_id = data['location_id'];
+     getLocationName(location_id);
     }
     populateRow();
    }
-   function populateRow(){
+
+   function getLocationName(location_id){
+    var url = apiProtocol + "://" + apiURL + ":" + apiPort + "/api/v1/locations/"+location_id;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
+        var obj = JSON.parse(this.responseText);
+        locationName = obj['name']; 
+        console.log(locationName);      
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
+    xhttp.setRequestHeader('Content-type', "application/json");
+    xhttp.send();
+    
+}
+
+
+  function populateRow(){
     var table = document.getElementById("user_stats");
     
     var new_row = document.createElement("tr");
@@ -117,6 +139,12 @@ $j(document).ready(function(){
     td_total.appendChild(node_total);
     new_row.appendChild(td_total);
 
+    var td_location = document.createElement("td");
+    td_location.style.width = "200px";
+
+    var node_location = document.createTextNode(locationName);
+    td_location.appendChild(node_location);
+    new_row.appendChild(td_location);
 
 
     table.appendChild(new_row);
